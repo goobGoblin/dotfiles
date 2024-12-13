46d14770e9144c24e1af5737ce3d8be7f0b41bd2
goobGoblin <56238853+recurse-that@users.noreply.github.com>
2024-12-12 19:36:41 -0600

    tmux

--- .config/lvim/config.lua
+++ .config/lvim/config.lua
@@ -18,8 +18,26 @@ lvim.plugins = {
         "folke/trouble.nvim", -- optional
         "nvim-telescope/telescope.nvim"
       }
+  },
+  { 
+    "christoomey/vim-tmux-navigator", 
+      cmd = {
+        "TmuxNavigateLeft",
+        "TmuxNavigateDown",
+        "TmuxNavigateUp",
+        "TmuxNavigateRight",
+        "TmuxNavigatePrevious",
+      },
+      keys = {
+        { "<c-h>", "<cmd><C-U>TmuxNavigateLeft<cr>" },
+        { "<c-j>", "<cmd><C-U>TmuxNavigateDown<cr>" },
+        { "<c-k>", "<cmd><C-U>TmuxNavigateUp<cr>" },
+        { "<c-l>", "<cmd><C-U>TmuxNavigateRight<cr>" },
+        { "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
+      }
   }
 }
+
 
 -- Read the docs: https://www.lunarvim.org/docs/configuration
 -- Example configs: https://github.com/LunarVim/starter.lvim
