3dd361eb1ce0d38b40bff1cfd6c9a43862692718
goobGoblin <56238853+recurse-that@users.noreply.github.com>
2024-12-20 19:25:20 -0600

    wezterm

---
+++ .config/wezterm/wezterm.lua
@@ -0,0 +1,78 @@
+-- Pull in the wezterm API
+local wezterm = require("wezterm")
+local act = wezterm.action
+-- This will hold the configuration.
+local config = wezterm.config_builder()
+
+-- This is where you actually apply your config choices
+
+config.color_scheme = 'Atom'
+
+-- config.font = wezterm.font("MesloLGS Nerd Font Mono")
+config.font_size = 15
+
+config.enable_tab_bar = false
+config.window_background_opacity = 0.75
+
+config.window_decorations = "RESIZE"
+
+-- config.macos_window_background_blur = 10
+
+-- keymaps
+config.keys = {	
+	{
+		key = "h",
+		mods = "CTRL|SHIFT|ALT",
+		action = wezterm.action.SplitPane({
+			direction = "Right",
+			size = { Percent = 50 },
+		}),
+	},
+	{
+		key = "v",
+		mods = "CTRL|SHIFT|ALT",
+		action = wezterm.action.SplitPane({
+			direction = "Down",
+			size = { Percent = 50 },
+		}),
+	},
+	{
+		key = "U",
+		mods = "CTRL|SHIFT",
+		action = act.AdjustPaneSize({ "Left", 5 }),
+	},
+	{
+		key = "I",
+		mods = "CTRL|SHIFT",
+		action = act.AdjustPaneSize({ "Down", 5 }),
+	},
+	{
+		key = "O",
+		mods = "CTRL|SHIFT",
+		action = act.AdjustPaneSize({ "Up", 5 }),
+	},
+	{
+		key = "P",
+		mods = "CTRL|SHIFT",
+		action = act.AdjustPaneSize({ "Right", 5 }),
+	},
+	{ key = "9", mods = "CTRL", action = act.PaneSelect },
+	{ key = "L", mods = "CTRL", action = act.ShowDebugOverlay },
+	{
+		key = "O",
+		mods = "CTRL|ALT",
+		-- toggling opacity
+		action = wezterm.action_callback(function(window, _)
+			local overrides = window:get_config_overrides() or {}
+			if overrides.window_background_opacity == 1.0 then
+				overrides.window_background_opacity = 0.75
+			else
+				overrides.window_background_opacity = 1.0
+			end
+			window:set_config_overrides(overrides)
+		end),
+	},
+}
+
+-- and finally, return the configuration to wezterm
+return config
