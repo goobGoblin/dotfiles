b1423c6542ae2b54c6f305fca1009f659f45de44
tofu <erb.zai@gmail.com>
2025-03-27 21:51:13 -0500

    Added install scripts for installing apps on new machine

---
+++ scripts/install_vim.sh
@@ -0,0 +1,12 @@
+# install neovim
+add-apt-repository ppa:neovim-ppa/stable
+apt install neovim
+
+# install lunarvim and dependencies
+apt install make pip python npm node cargo ripgrep
+LV_BRANCH='release-1.4/neovim-0.9' bash <(curl -s https://raw.githubusercontent.com/LunarVim/LunarVim/release-1.4/neovim-0.9/utils/installer/install.sh)
+
+# add nerd fonts
+mkdir -p ~/.local/share/fonts
+cd ~/.local/share/fonts && curl -fLO https://github.com/ryanoasis/nerd-fonts/raw/HEAD/patched-fonts/DroidSansMono/DroidSansMNerdFont-Regular.otf
+cd 
