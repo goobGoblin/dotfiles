ddc641091546fbd9b5982171b03aee2a67b91eb8
goobGoblin <erb.zai@gmail.com>
2025-04-05 09:32:08 -0500

    updated

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -64,10 +64,10 @@ config.keys = {
 		-- toggling opacity
 		action = wezterm.action_callback(function(window, _)
 			local overrides = window:get_config_overrides() or {}
-			if overrides.window_background_opacity == 1.0 then
-				overrides.window_background_opacity = 0.75
+			if overrides.window_background_opacity == 0.75 then
+				overrides.window_background_opacity = 1.00
 			else
-				overrides.window_background_opacity = 1.0
+				overrides.window_background_opacity = 0.75
 			end
 			window:set_config_overrides(overrides)
 		end),
