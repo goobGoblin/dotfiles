2238666433e8d556b3020f3a8b8e22e146246e57
ZaiErb <58526166+goobGoblin@users.noreply.github.com>
2025-05-07 11:48:31 -0500

    Update install_vim.sh

--- scripts/install_vim.sh
+++ scripts/install_vim.sh
@@ -3,7 +3,8 @@ add-apt-repository ppa:neovim-ppa/stable
 apt install neovim
 
 # install lunarvim and dependencies
-apt install make pip python npm node cargo ripgrep
+apt install make python3-pip python3 npm nodejs cargo ripgrep curl rustup
+rustup update stable
 LV_BRANCH='release-1.4/neovim-0.9' bash <(curl -s https://raw.githubusercontent.com/LunarVim/LunarVim/release-1.4/neovim-0.9/utils/installer/install.sh)
 
 # add nerd fonts
